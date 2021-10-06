c515a6d5a7a85c2c190e35658259521b74c79449
SebastianMorado <sabby.morado@gmail.com>
2021-10-06 15:26:43 +0800

    fix bugs

--- Chat iOS/PushNotificationSender.swift
+++ Chat iOS/PushNotificationSender.swift
@@ -11,30 +11,35 @@ import UIKit
 class PushNotificationSender {
     
     func sendPushNotification(to token: String, title: String, body: String) {
+        print("HEREEEEEEE")
         let urlString = "https://fcm.googleapis.com/fcm/send"
-        let url = NSURL(string: urlString)!
-        let paramString: [String : Any] = ["to" : token,
-                                           "notification" : ["title" : title, "body" : body],
-                                           "data" : ["user" : "test_id"]
-        ]
-        let request = NSMutableURLRequest(url: url as URL)
-        request.httpMethod = "POST"
-        request.httpBody = try? JSONSerialization.data(withJSONObject:paramString, options: [.prettyPrinted])
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.setValue("key=AAAAleC2sOc:APA91bE7KwcU8TSXbFpfU3pVrFtS9PyMcY1nWMoQPypMMKLTQeVG1MKA28yWwdFKNvlbFlCy3Kbs0o-6yNH-jtz92rGvbPJYVLp19ZHPGZvvO2aTVy5yEIJcuSSg6lHUdiKntNXHI1Yj", forHTTPHeaderField: "Authorization")
-        
-        let task =  URLSession.shared.dataTask(with: request as URLRequest)  { (data, response, error) in
-            do {
-                if let jsonData = data {
-                    if let jsonDataDict  = try JSONSerialization.jsonObject(with: jsonData, options: JSONSerialization.ReadingOptions.allowFragments) as? [String: AnyObject] {
-                        NSLog("Received data:\n\(jsonDataDict))")
+        if let apiKey = Bundle.main.infoDictionary?["CLOUD_MESSAGING_API_KEY"] as? String, apiKey != "" {
+            let url = NSURL(string: urlString)!
+            let paramString: [String : Any] = ["to" : token,
+                                               "notification" : ["title" : title, "body" : body],
+                                               "data" : ["user" : "test_id"]
+            ]
+            let request = NSMutableURLRequest(url: url as URL)
+            request.httpMethod = "POST"
+            request.httpBody = try? JSONSerialization.data(withJSONObject:paramString, options: [.prettyPrinted])
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.setValue("key=" + apiKey, forHTTPHeaderField: "Authorization")
+            
+            let task =  URLSession.shared.dataTask(with: request as URLRequest)  { (data, response, error) in
+                do {
+                    if let jsonData = data {
+                        if let jsonDataDict  = try JSONSerialization.jsonObject(with: jsonData, options: JSONSerialization.ReadingOptions.allowFragments) as? [String: AnyObject] {
+                            NSLog("Received data:\n\(jsonDataDict))")
+                        }
                     }
+                } catch let err as NSError {
+                    print(err.debugDescription)
                 }
-            } catch let err as NSError {
-                print(err.debugDescription)
             }
+            task.resume()
+        } else {
+            print("no key :(")
         }
-        task.resume()
     }
     
     
